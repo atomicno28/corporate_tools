0878cc15b054a5675c57d3511d47429aa91a3418
Nikhil Patel <nikhilid2205@gmail.com>
2023-11-15 19:17:24 +0530

    App and Index

---
+++ corporate/src/App.js
@@ -0,0 +1,14 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import Homepage from "./components/Homepage";
+import Superpower from "./components/Superpower";
+
+function App() {
+  return (
+    <>
+      <Homepage />
+      <Superpower />
+    </>
+  );
+}
+
+export default App;
