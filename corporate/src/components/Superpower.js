a11eb9e120135a7eacdfeff4f6ce4c28d6fed774
Nikhil Patel <nikhilid2205@gmail.com>
2023-11-15 19:56:25 +0530

    Adding the SuperPower css

--- corporate/src/components/Superpower.js
+++ corporate/src/components/Superpower.js
@@ -4,21 +4,21 @@ import { Card } from "react-bootstrap";
 const data = [
   {
     id: 1,
-    link: "source",
+    link: "https://w7.pngwing.com/pngs/1003/487/png-transparent-github-pages-random-icons-white-logo-monochrome-thumbnail.png",
     Topic: "Super Power 1",
     Content:
       "Lorem ipsum praesent massa at ligula reet ist icuqilla. Vivamius est mist aliquit elit milt ac nisl.",
   },
   {
     id: 2,
-    link: "source",
+    link: "https://w7.pngwing.com/pngs/1003/487/png-transparent-github-pages-random-icons-white-logo-monochrome-thumbnail.png",
     Topic: "Super Power 2",
     Content:
       "Lorem ipsum praesent massa at ligula reet ist icuqilla. Vivamius est mist aliquit elit milt ac nisl.",
   },
   {
     id: 3,
-    link: "source",
+    link: "https://w7.pngwing.com/pngs/1003/487/png-transparent-github-pages-random-icons-white-logo-monochrome-thumbnail.png",
     Topic: "Super Power 3",
     Content:
       "Lorem ipsum praesent massa at ligula reet ist icuqilla. Vivamius est mist aliquit elit milt ac nisl.",
@@ -30,15 +30,25 @@ const renderedData = data.map((item) => (
     key={item.id}
     style={{
       width: "23rem",
-      height: "22rem",
+      height: "23rem",
       marginRight: "25px",
-      background: `linear-gradient(to right, darkblue, blue, blue)`,
+      background: `linear-gradient(to right, #191970,#000080)`,
+      borderRadius: "20px",
     }}
   >
-    <Card.Img variant="top" src={item.link} />
+    <Card.Img
+      variant="top"
+      src={item.link}
+      height="170px"
+      style={{ padding: "30px" }}
+    />
     <Card.Body>
-      <Card.Title style={{ color: "white" }}>{item.Topic}</Card.Title>
-      <Card.Text>{item.Content}</Card.Text>
+      <Card.Title style={{ color: "white", fontSize: "1.7rem" }}>
+        <b>{item.Topic}</b>
+      </Card.Title>
+      <Card.Text style={{ color: "white", fontSize: "1.4rem" }}>
+        {item.Content}
+      </Card.Text>
     </Card.Body>
   </Card>
 ));
@@ -53,8 +63,14 @@ function Superpower() {
           height: "100%",
         }}
       >
-        <h2 style={{ padding: "90px 90px 0px 90px", marginBottom: "0px" }}>
-          We give you these 3 superpowers!
+        <h2
+          style={{
+            padding: "90px 90px 0px 90px",
+            marginBottom: "0px",
+            fontSize: "2.5rem",
+          }}
+        >
+          <b> We give you these 3 superpowers!</b>
         </h2>
 
         <div
